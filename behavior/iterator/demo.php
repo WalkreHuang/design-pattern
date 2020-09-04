cf32bc527aa6b2f858366268dc4a8062db978888
walkerhuang <huangwalker@qq.com>
2020-09-04 16:32:20 +0800

    init

---
+++ behavior/iterator/demo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * User: huangwalker
+ * Date: 2020/9/4
+ * Time: 14:41
+ * Email: <huangwalker@qq.com>
+ */
+Interface Iterator
+{
+    public function hasNext();
+
+    public function next();
+
+    public function currentItem();
+}
+
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+## personal design-pattern in geek-time
